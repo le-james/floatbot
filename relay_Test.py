aa78acfdac2e4db77b95a42aa8a5bd3f52561105
James Le <le_james@outlook.com>
2022-05-22 18:46:08 -0700

    old manual control code

--- relay_Test.py
+++ relay_Test.py
@@ -1,30 +1,37 @@
 import RPi.GPIO as GPIO
 import time
 
+# thruster gpio pins
+thrusters = [17, 27, 22, 10, 9, 11, 0, 5]
+
+# setup thruster gpio pins
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(thrusters, GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.output(9, GPIO.HIGH)
+GPIO.output(27, GPIO.HIGH)
+print("hi")
+time.sleep(1)
+print("after sleep")
+GPIO.output(9, GPIO.LOW)
+GPIO.output(27, GPIO.LOW)
+print("low")
 
 
-#relay 1
-GPIO.setup(2, GPIO.OUT)
-#relay 2
-GPIO.setup(3, GPIO.OUT)
+GPIO.cleanup()
 
-try:
-    while True:
-        GPIO.output(2, GPIO.HIGH)
-        print('Relay 1 ON')
-        time.sleep(2)
-        GPIO.output(3, GPIO.HIGH)
-        print('Relay 2 ON')
-        time.sleep(2)
-        GPIO.output(2, GPIO.LOW)
-        print('Relay 1 OFF')
-        time.sleep(2)
-        GPIO.output(3, GPIO.LOW)
-        print('Relay 2 OFF')
-        time.sleep(2)
-except KeyboardInterrupt:   #ctrl-c
-    print("Cleaning up GPIO")
-finally:
-    GPIO.cleanup()
+# try:
+#     while True:
+#         GPIO.output(pin1, GPIO.HIGH)
+#         print('Relay 1 ON')
+#         time.sleep(2)
+
+#         GPIO.output(pin1, GPIO.LOW)
+#         print('Relay 1 OFF')
+#         time.sleep(2)
+
+# except KeyboardInterrupt:   #ctrl-c
+#     print("Cleaning up GPIO")
+# finally:
+#     GPIO.cleanup()
 
