aa6947b8f2ec406098cb8b2311624d4740aa1694
James Le <le_james@outlook.com>
2022-05-26 22:32:49 -0700

    add in simple controller and user input code

--- floatbot_gnc_3.py
+++ floatbot_gnc_3.py
@@ -5,6 +5,8 @@ from cmath import pi
 import numpy as np
 import threading
 import time
+import keyboard
+
 
 """ set up imu gyro """
 mpu = mpu6050(0x68)
@@ -18,7 +20,7 @@ print("Calibrating Gyro Yaw (z-axis)... ")
 count = 0
 gyroReadings = 420     # number of gyro data to pull
 sumZGyroReadings = 0
-while True:
+while True:     # calibrate gyro
     # counter to end while loop
     count += 1
 
@@ -40,7 +42,6 @@ while True:
 
 
 """ FLOATBOT NAVIGATION THREAD FUNCTION """
-
 # position and angle of floatbot
 floatbot_pose = np.zeros(3)
 
@@ -93,8 +94,8 @@ def floatbot_navigation():
         prev_gryo_data = gyro_data['z']-gyroZBias
 
         # store pose in array
-        floatbot_pose[0] = x
-        floatbot_pose[1] = y
+        floatbot_pose[0] = x*100    # convert to cm
+        floatbot_pose[1] = y*100    # convert to cm
         floatbot_pose[2] = yawAng
         # print("x: ", floatbot_pose[0], "y: ", floatbot_pose[1], "yaw: ", floatbot_pose[2])
         
@@ -112,54 +113,62 @@ print("Started Floatbot navigation!")
 
 
 
-""" PID CONTROLLER """
-# pid parameters
-kpp = 0.75      # proportional postion gain
-kpy = 0.01      # proportional yaw gain
-ki = 1
-kd = 1
-intError = 0
-lastError = 0
-prevT = np.zeros(8)
-
-def pid(nav, ref):   
-    global intError, lastError, prevT
+""" SIMPLE TRANSLATIONAL CONTROLLER """
+def xy_controller(nav, ref):   
 
     # pose error
-    error = nav - ref 
-                         
-    # store control inputs
-    u = np.zeros(3)
+    error = nav - ref                      
+
+    # 1-norm (error)
+    dis_error = np.array([error[0], error[1]])
+
+    # psudo control action
+    u = dis_error
+
+    # xy thruster matrix reaction force
+    thruster_matrix = np.array([[-1, 1, 0, 0, 1, -1, 0, 0], 
+                                [0, 0, 1, -1, 0, 0, -1, 1]])
+    
+    # psudoinverse of thruster matrix
+    mInv = np.linalg.pinv(thruster_matrix)
+    
+    # compute the inverse of u=mt, where m is the thruster matrix
+    t = np.matmul(mInv, u)  # dont need to multiply by 2
+
+    # thrusters to fire
+    t[t<0] = 0
+    t[t>0] = 0.1
+
+    return t    # function returns the PID output
+""" SIMPLE TRANSLATIONAL CONTROLLER """
+
+""" SIMPLE YAW CONTROLLER """
+def yaw_controller(nav, ref):   
+
+    # pose error
+    error = nav - ref                      
 
     # distance vector
-    dis = np.array([error[0], error[1]])
+    yaw_error = error[2]
 
-    if np.linalg.norm(dis) > 0.03 or np.abs(error[2]*pi/180) > 5*pi/180:
-        # # pid error terms
-        # intError += error*dt                  # compute integral
-        # rateError = (error - lastError)/dt    # compute derivative
+    # psudo control action
+    u = yaw_error
 
-        # pid output
-        # u = kp*error #+ ki*intError + kd*rateError 
-        u[0] = kpp*error[0]
-        u[1] = kpp*error[1]
-        
-        # location of each matric on floatbot
-        # from serc gnc paper
-        thruster_matrix = np.array([[-1, 1, 0, 0, 1, -1, 0, 0], 
-                                    [0, 0, 1, -1, 0, 0, -1, 1]])
-        
-        # psudoinverse of thruster matrix
-        mInv = np.linalg.pinv(thruster_matrix)
-        
-        # time for each thruster
-        t = 2*np.matmul(mInv, u)#.round(decimals=2) # dont need to round since bounds takes care of it below
+    # yaw thruster matrix reaction torque
+    thruster_matrix = np.array([[-1, 1, -1, 1, -1, 1, -1, 1]])
+    
+    # psudoinverse of thruster matrix
+    mInv = np.linalg.pinv(thruster_matrix)
+    
+    # compute the inverse of u=mt, where m is the thruster matrix
+    t = np.matmul(mInv, u)  # dont need to multiply by 2
 
-    else:
-        print("SOMETHING WRONG IN PID FUNCTION!")
+    # thrusters to fire
+    t[t<0] = 0
+    t[t>0] = 0.05
 
-    return t 
-""" PID CONTROLLER """
+    return t    # function returns the PID output
+""" SIMPLE YAW CONTROLLER """
 
 
 
@@ -172,7 +181,6 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(thrusters, GPIO.OUT, initial=GPIO.HIGH)  # relays OFF - reverse logic for some reason
 
 def fire_thruster(pin, name, fireTime):
-    # dt = fireTime if fireTime >= 0.1 else 0 - handled in pid thread already
     
     GPIO.output(pin, GPIO.LOW)
     # print(name, 'ON')
@@ -199,29 +207,93 @@ def thruster_control(fireTimes):
         t.join()
 """ THRUST CONTROL """
 
-# NEED TO WRITE SOME USER INTERFACE CODE HERE
-# reference point
-ref = [0.92, 0.63, 90]     # [meter, meter, deg]
 
+
+""" USER SET POINT """
+ref = [0, 0, 0]
+
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+print("Enter where the Floatbot should go and press enter...")
+ref[0] = input("x position [cm]: ")
+ref[1] = input("y position [cm]: ")
+ref[2] = input("yaw angle [deg]: ")
+
+check = ref     # copy shape of ref
+
+for idx, val in enumerate(ref):
+    check[idx] = isfloat(val)   
+# print("check ", check)
+# print(all(check))
+
+# checks if user input is valid
+# userInCheck = True   # in user input mode
+def setpoint():    # user set the goal pose of the floatbot
+    while True:
+        for idx, val in enumerate(check):
+            if val == False:
+                if idx == 0:
+                    print("Re-enter x postion again...")
+                    ref[idx] = input("x position [cm]: ")
+                    check[idx] = isfloat(ref[idx])   
+                elif idx == 1:
+                    print("Re-enter y postion again...")
+                    ref[idx] = input("y position [cm]: ")
+                    check[idx] = isfloat(ref[idx])   
+                elif idx == 2:
+                    print("Re-enter yaw angle again...")
+                    ref[idx] = input("yaw angle [deg]: ")
+                    check[idx] = isfloat(ref[idx])   
+
+        if all(check) == True:
+            # convert into float and meter values
+            ref[0] = float(ref[0])/100
+            ref[1] = float(ref[1])/100
+            ref[2] = float(ref[2])
+            # print out where the floatbot will move to
+            print("The Floatbot will move to: " + "(" , ref[0], "," , ref[1], ")" 
+                    + " with angle: ", ref[2], " [deg]")
+            # userInCheck = False
+            break
+setpoint()
+""" USER SET POINT """
+
+
+
+""" CONTROL LOOP """
 # control loop parameters
 h = 0.2   # 5Hz control loop 
 
-print("STARTING CONTROL LOOP")
+print("Press the 'tab' key to start control loop")
+keyboard.wait('tab')
+print("STARTING CONTROL LOOP...")
+
 try:
     while True:
         # testing naviation thread - WORKS BUT USB PORT OF THE MOBILE BEACON CHANGES
         # print("inside control loop")
         print("x: ", floatbot_pose[0], "y: ", floatbot_pose[1], "yaw: ", floatbot_pose[2])
 
-        # thruster control
+        # translational motion
+        t = xy_controller(floatbot_pose, ref)
         thruster_control(t)
 
-        # time.sleep(dt+1)
-        time.sleep(3)
-        # time.sleep(1)
+        # yaw motion
+        t = yaw_controller(floatbot_pose, ref)
+        thruster_control(t)
 
+
+
+
+        time.sleep(3)
 except KeyboardInterrupt:
     GPIO.cleanup()
     print("CLEANED UP GPIO")
     time.sleep(1)
     print('TERMINATED CONTROL LOOP')
+""" CONTROL LOOP """
