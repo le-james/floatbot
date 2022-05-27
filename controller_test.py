cfe46f1f686aefc49640059ff08b1ae96199a600
James Le <le_james@outlook.com>
2022-05-26 22:31:14 -0700

    fix valve open time translation and yaw controller

--- controller_test.py
+++ controller_test.py
@@ -1,38 +1,68 @@
 from cmath import pi
 import numpy as np
 
-""" SIMPLE CONTROLLER """
-def controller(nav, ref):   
+""" SIMPLE TRANSLATIONAL CONTROLLER """
+def xy_controller(nav, ref):   
 
     # pose error
     error = nav - ref                      
-    error[2] = error[2]*pi/180
 
-    # distance vector
-    dis = np.array([error[0], error[1]])
+    # 1-norm (error)
+    dis_error = np.array([error[0], error[1]])
 
     # psudo control action
-    u = dis
+    u = dis_error
 
-    # location of each matric on floatbot
+    # xy thruster matrix reaction force
     thruster_matrix = np.array([[-1, 1, 0, 0, 1, -1, 0, 0], 
                                 [0, 0, 1, -1, 0, 0, -1, 1]])
     
     # psudoinverse of thruster matrix
     mInv = np.linalg.pinv(thruster_matrix)
     
-    # time for each thruster
-    t = 2*np.matmul(mInv, u)#.round(decimals=2)
+    # compute the inverse of u=mt, where m is the thruster matrix
+    t = np.matmul(mInv, u)  # dont need to multiply by 2
 
     # thrusters to fire
-    t[0 if condition else b] = 1
+    t[t<0] = 0
+    t[t>0] = 0.1
 
     return t    # function returns the PID output
-""" SIMPLE CONTROLLER """
+""" SIMPLE TRANSLATIONAL CONTROLLER """
 
-nav = np.array([[1], [1], [0]])     # pose from gps and imu
+""" SIMPLE YAW CONTROLLER """
+def yaw_controller(nav, ref):   
+
+    # pose error
+    error = nav - ref                      
+
+    # distance vector
+    yaw_error = error[2]
+
+    # psudo control action
+    u = yaw_error
+
+    # yaw thruster matrix reaction torque
+    thruster_matrix = np.array([[-1, 1, -1, 1, -1, 1, -1, 1]])
+    
+    # psudoinverse of thruster matrix
+    mInv = np.linalg.pinv(thruster_matrix)
+    
+    # compute the inverse of u=mt, where m is the thruster matrix
+    t = np.matmul(mInv, u)  # dont need to multiply by 2
+
+    # thrusters to fire
+    t[t<0] = 0
+    t[t>0] = 0.05
+
+    return t    # function returns the PID output
+""" SIMPLE YAW CONTROLLER """
+
+# test output of controller
+nav = np.array([[1], [1], [45]])    # pose from gps and imu
 ref = np.array([[0], [0], [0]])     # where we want to go
 
-t = controller(nav, ref)
-
+t = xy_controller(nav, ref)
+print(t)
+t = yaw_controller(nav, ref)
 print(t)
